253dcae2efbe62dcf202aec574bd6319f006909c
Aaron Shao <72823620+shaoaaron@users.noreply.github.com>
2024-10-26 17:57:29 +0200

    created the image array

--- imageConverter.py
+++ imageConverter.py
@@ -2,4 +2,26 @@ import cv2
 import numpy as np
 
 img = cv2.imread("MAP1.png",cv2.IMREAD_COLOR)
+height, width, channel = img.shape
+
+map = []
+black = [0,0,0]
+white = [255,255,255]
+green = [0,255,0]
+red = [0,0,255]
+
+for x in range(height):
+    line = []
+    for y in range(width):
+        pixel = int(img[x, y])
+        if pixel == black:
+            line.append(1)
+        elif pixel == white:
+            line.append(0)
+        elif pixel == green:
+            line.append(2)
+        elif pixel == red:
+            line.append(3)
+    map.append(line)
+
 
