3e2fdeced886d1f96c432473d4282a42ba8fc376
Aaron Shao <72823620+shaoaaron@users.noreply.github.com>
2024-10-26 18:26:14 +0200

    convert image array to file

--- imageConverter.py
+++ imageConverter.py
@@ -5,10 +5,14 @@ img = cv2.imread("MAP1.png",cv2.IMREAD_COLOR)
 height, width, channel = img.shape
 
 map = []
+
 black = [0,0,0]
 white = [255,255,255]
 green = [0,255,0]
 red = [0,0,255]
+
+start = []
+end = []
 
 for x in range(height):
     line = []
@@ -20,8 +24,16 @@ for x in range(height):
             line.append(0)
         elif pixel == green:
             line.append(2)
+            start.append(x,y)
         elif pixel == red:
             line.append(3)
+            end.append(x,y)
     map.append(line)
+
+np.save("MAP1array.npy")
+
+with open('positions.txt', 'w') as f:
+    f.write(f"{start[0]} {start[1]}\n")
+    f.write(f"{end[0]} {end[1]}\n")
 
 
